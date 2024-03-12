4dbbc97f0fd23a0fea2b6d3f940f3b45e9e80301
Quinntyx <harrisonhenry916@gmail.com>
2024-03-12 15:07:42 -0500

    added automation system

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::io::{stdin, Write};
-use std::io;
 use kalosm::language::*;
+use std::collections::VecDeque;
+use std::fs;
+use std::io;
+use std::io::{stdin, Write};
 
-fn print (s: &str) -> io::Result<()> {
+use ron::{de, ser};
+
+fn print(s: &str) -> io::Result<()> {
     print!("{}", s);
     std::io::stdout().flush()
 }
 
-fn build (message: (bool, String)) -> String {
+fn build(message: (bool, String)) -> String {
     if message.0 {
         format!("<|user|>{}</s>", message.1)
     } else {
@@ -15,17 +19,37 @@ fn build (message: (bool, String)) -> String {
     }
 }
 
+fn write_next_message_content(msg: &mut String, msg_queue: &mut VecDeque<String>) {
+    msg_queue
+        .pop_front()
+        .map(|a| {
+            println!("{a}");
+            *msg = a
+        })
+        .unwrap_or_else(|| {
+            stdin().read_line(msg).unwrap();
+        });
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     println!("Initializing Zephyr 7B Alpha (Llama)... ");
-    
-    let llm = LlamaBuilder::default().with_source(LlamaSource::zephyr_7b_alpha()).build()?;
+
+    let llm = LlamaBuilder::default()
+        .with_source(LlamaSource::zephyr_7b_alpha())
+        .build()?;
     println!("done!");
+
+    let mut msgq = fs::read_to_string("msgq.ron")
+        .map(|m| de::from_str(&m).unwrap())
+        .unwrap_or_else(|_| VecDeque::new());
 
     let base_prompt = String::from(
         "<|system|>Your name is Fiona. You are extremely smart, helpful, and take pride in having knowledge and solving difficult problems. You are also uptight \
         and curt with responses.</s>"
     );
+
+    let mut old_messages: Vec<(bool, String)> = Vec::new();
 
     let mut messages: Vec<(bool, String)> = Vec::new();
 
@@ -33,28 +57,43 @@ async fn main() -> anyhow::Result<()> {
 
     loop {
         let mut message = String::new();
-        
+
         print(&format!("[{current}] User: "))?;
 
-        stdin().read_line(&mut message)?;
+        write_next_message_content(&mut message, &mut msgq);
+
         message.remove(message.len() - 1); // always a newline
 
-        if message.contains("EXIT") { break; }
+        if message.contains("EXIT") {
+            break;
+        }
+
+        if message.contains("RESET") {
+            println!("Resetting!");
+            old_messages.append(&mut messages);
+
+            fs::write(
+                "conversation.ron",
+                ser::to_string_pretty(&old_messages, Default::default()).unwrap(),
+            )?;
+        }
 
         messages.push((true, message.clone()));
 
         current += 1;
 
         let prompt = format!(
-            "{}\n{}\n<|assistant|>", 
+            "{}\n{}\n<|assistant|>",
             base_prompt,
-            &messages.iter()
+            &messages
+                .iter()
                 .map(|m| build(m.clone()))
                 .collect::<Vec<String>>()
                 .join("\n")
         );
 
-        let stream = llm.stream_text(&prompt)
+        let stream = llm
+            .stream_text(&prompt)
             .with_stop_on(Some(String::from("</s>")))
             .await
             .unwrap();
@@ -79,7 +118,14 @@ async fn main() -> anyhow::Result<()> {
         current += 1;
     }
 
+    old_messages.append(&mut messages);
+
     println!("Exiting!");
+
+    fs::write(
+        "conversation.ron",
+        ser::to_string_pretty(&old_messages, Default::default()).unwrap(),
+    )?;
 
     Ok(())
 }
